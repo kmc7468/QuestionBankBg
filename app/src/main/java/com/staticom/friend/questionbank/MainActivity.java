10a7676e8c6e90b5bb8f1d028d9f9489027c84e0
kmc7468 <kmc7468@naver.com>
2017-11-27 19:38:04 +0900

    Add files

---
+++ app/src/main/java/com/staticom/friend/questionbank/MainActivity.java
@@ -0,0 +1,141 @@
+package com.staticom.friend.questionbank;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.AppOpsManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileReader;
+
+import javax.xml.transform.OutputKeys;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            int READ_WRITE_permission = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            if (READ_WRITE_permission != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(getApplicationContext(), "권한을 부여해 주세요. 권한이 없으면 앱은 종료됩니다. (문제은행 앱의 잠금 해제를 위해 필요한 권한입니다.)", Toast.LENGTH_SHORT).show();
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // 승인됨
+                } else {
+                    moveTaskToBack(true);
+                    finish();
+                    android.os.Process.killProcess(android.os.Process.myPid());
+                }
+                break;
+
+            case 2: // PACKAGE_USAGE_STATS
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // 승인됨
+                } else {
+                    moveTaskToBack(true);
+                    finish();
+                    android.os.Process.killProcess(android.os.Process.myPid());
+                }
+                break;
+        }
+    }
+
+    public void end(View view) {
+        try {
+            File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath() + "/영구해제.txt");
+            if (file.exists()) {
+                FileReader reader = new FileReader(file);
+                char[] buf = new char[20];
+                reader.read(buf);
+                String str = new String(buf);
+                if (str.contains("off")) {
+                    reader.close();
+                    file.delete();
+                    if (service != null) {
+                        stopService(service);
+                    }
+                    android.os.Process.killProcess(android.os.Process.myPid());
+                    return;
+                }
+            }
+
+            file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath() + "/작동중.txt");
+
+            if(!file.exists()) {
+                android.os.Process.killProcess(android.os.Process.myPid());
+                return;
+            }
+        } catch (Exception e) {
+
+        }
+        Toast.makeText(getApplicationContext(), "종료에 실패했습니다. (오류가 발생했거나, 학부모 권한 없이 문제은행 앱이 종료되었습니다.)", Toast.LENGTH_SHORT).show();
+    }
+
+    private static Intent service = null;
+
+    public void shell(View view) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            boolean granted = false;
+
+            AppOpsManager appOps = (AppOpsManager) getSystemService(Context.APP_OPS_SERVICE);
+            int mode = appOps.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS,android.os.Process.myUid(), getPackageName());
+
+            if (mode == AppOpsManager.MODE_DEFAULT) {
+                granted = (checkCallingOrSelfPermission(android.Manifest.permission.PACKAGE_USAGE_STATS) == PackageManager.PERMISSION_GRANTED);
+            } else {
+                granted = (mode == AppOpsManager.MODE_ALLOWED);
+            }
+
+            if (!granted)
+            {
+                Toast.makeText(getApplicationContext(), "QuestionBank에 권한을 부여해 주세요. 그 후 버튼을 다시 눌러주세요.", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(android.provider.Settings.ACTION_USAGE_ACCESS_SETTINGS);
+                startActivity(intent);
+                return;
+            }
+
+            int READ_WRITE_permission = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+            if (!granted || READ_WRITE_permission != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(getApplicationContext(), "필요한 권한이 없어 문제은행을 실행할 수 없습니다. 이 앱을 껐다가 다시 켜주세요.", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
+
+        try {
+            File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath() + "/작동중.txt");
+            file.createNewFile();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "오류가 발생했습니다. 이 앱을 껐다가 다시 켜신 후 다시 시도해주세요.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        service = new Intent(this, QuestionBank.class);
+        startService(service);
+        moveTaskToBack(true);
+        finish();
+    }
+}
---
+++ app/src/main/java/com/staticom/friend/questionbank/QuestionBank.java
@@ -0,0 +1,183 @@
+package com.staticom.friend.questionbank;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.Service;
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.IBinder;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.concurrent.TimeUnit;
+
+public class QuestionBank extends Service {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    private Handler toast;
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        toast = new Handler();
+
+        Intent questionBankOrg = getPackageManager().getLaunchIntentForPackage("appinventor.ai_youngjaewon2017.Question_Bank_A_");
+        startActivity(questionBankOrg);
+
+        thread = new Thread(task);
+        thread.start();
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onDestroy() {
+        thread.stop();
+        super.onDestroy();
+    }
+
+    private class ToastRunnable implements Runnable {
+        private String message;
+
+        public ToastRunnable(String text) {
+            message = text;
+        }
+
+        @Override
+        public void run() {
+            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private Context c = this;
+
+    private Thread thread = null;
+    private Runnable task = new Runnable() {
+
+        private char[] buf = new char[20];
+        private static final String pack = "appinventor.ai_youngjaewon2017.Question_Bank_A_";
+        private long i = 0;
+        private File f_off = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath() + "/잠금해제.txt");
+        private File f_real_off = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath() + "/영구해제.txt");
+        @Override
+        public void run() {
+            boolean _run = true;
+            while (_run) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+                    UsageStatsManager mUsageStatsManager = (UsageStatsManager)getSystemService(Context.USAGE_STATS_SERVICE);
+                    long endTime = System.currentTimeMillis();
+                    long beginTime = endTime - 1000*1000;
+                    String running_pack = null;
+
+                    List<UsageStats > stats = mUsageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, beginTime, endTime);
+
+                    String topActivity = null;
+
+                    if(stats != null)
+                    {
+                        SortedMap<Long,UsageStats> mySortedMap = new TreeMap<Long,UsageStats>();
+                        for (UsageStats usageStats : stats)
+                        {
+                            mySortedMap.put(usageStats.getLastTimeUsed(),usageStats);
+                        }
+                        if(mySortedMap != null && !mySortedMap.isEmpty())
+                        {
+                            topActivity =  mySortedMap.get(mySortedMap.lastKey()).getPackageName();
+                        }
+                    }
+
+                    if(topActivity != null && !topActivity.equals(pack)) {
+                        Intent questionBankOrg = getPackageManager().getLaunchIntentForPackage("appinventor.ai_youngjaewon2017.Question_Bank_A_");
+                        startActivity(questionBankOrg);
+                    }
+                } else {
+                    ActivityManager am = (ActivityManager)getSystemService(Context.ACTIVITY_SERVICE);
+
+                    List<ActivityManager.RunningAppProcessInfo> processes = am.getRunningAppProcesses();
+                    for (ActivityManager.RunningAppProcessInfo process : processes) {
+                        if (process == null) continue;
+                        if (process.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND) {
+                            if (!pack.equals(process.processName)) {
+                                Intent questionBankOrg = getPackageManager().getLaunchIntentForPackage("appinventor.ai_youngjaewon2017.Question_Bank_A_");
+                                startActivity(questionBankOrg);
+                            }
+                            break;
+                        }
+                    }
+                }
+
+                boolean stop_thread = false;
+
+                try {
+                    ++i;
+
+                    if (f_off.exists()) {
+                        FileReader reader = new FileReader(f_off);
+                        reader.read(buf);
+                        String str_tmp = new String(buf);
+                        if (str_tmp.contains("off")) {
+                            stop_thread = true;
+                            reader.close();
+                            f_off.delete();
+                        } else {
+                            reader.close();
+                        }
+                    }
+
+                    if (f_real_off.exists()) {
+                        Arrays.fill(buf, '\0');
+                        FileReader reader = new FileReader(f_real_off);
+                        reader.read(buf);
+                        String str_tmp = new String(buf);
+                        if (str_tmp.contains("off")) {
+                            _run = false;
+                        } else {
+                            Arrays.fill(buf, '\0');
+                        }
+                        reader.close();
+                    }
+
+                    if (i == Long.MAX_VALUE) {
+                        i = 0;
+                    }
+
+                    if (stop_thread) {
+                        stop_thread = false;
+                        Thread.sleep(1800000);
+                    } else {
+                        Thread.sleep(1000);
+                    }
+                } catch (Exception e) {
+                    String message = e.toString();
+                    message.equals("");
+                }
+            }
+
+            File new_file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath() + "/작동중.txt");
+            new_file.delete();
+
+            Intent intent = new Intent(c, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        }
+    };
+}
